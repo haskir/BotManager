7dd96d716709dc5b54b8026be2075ad4d0da14b5
haskird2 <haskird2@gmail.com>
2023-05-24 08:39:10 +0300

    )))

--- Process.py
+++ Process.py
@@ -6,6 +6,7 @@ import threading
 
 class Process:
     LAST = 0
+    enc = "cp866"
 
     @staticmethod
     def __make_filter(name, level):
@@ -55,12 +56,14 @@ class Process:
 
     def start(self):
         self.__stop = False
-        self.thr = threading.Thread(target=self.__start_proc).start()
+        threading.Thread(target=self.__logging).start()
+        self.__process = self.__start_proc()
         self.status = "running"
         self.on_start()
 
     def stop(self):
         self.__stop = True
+        self.__process.send_signal(0)
         self.status = "stopped"
         self.on_stop()
 
@@ -68,13 +71,12 @@ class Process:
         self.stop()
         self.start()
 
-    @logger.catch
     def update(self):
         self.stop()
         updater = subprocess.Popen(f"git pull {self.exec_path}", stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE,
                                    shell=True,
-                                   encoding=subprocess.getoutput("chcp").removeprefix("Active code page: "),
+                                   encoding=self.enc,
                                    errors='replace')
         while True:
             realtime_output = updater.stdout.readline()
@@ -85,15 +87,13 @@ class Process:
                 self._log.info(realtime_output.strip())
         [self.on_error(error) for error in updater.stderr.readlines() if error]
 
-    def __start_proc(self) -> subprocess:
-        threading.Thread(target=self.__logging).start()
-        self.__process = subprocess.Popen(
-            self.exec_path,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True,
-            encoding=subprocess.getoutput("chcp").removeprefix("Active code page: "),
-            errors='replace')
+    def __start_proc(self) -> subprocess.Popen:
+        return subprocess.Popen(self.exec_path,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                shell=True,
+                                encoding=self.enc,
+                                errors='replace')
 
     def __logging(self):
         while not self.__stop:
@@ -114,15 +114,15 @@ class Process:
 
 if __name__ == "__main__":
     n = 20
-    ping1 = Process(name="Ping1", exec_path=f"ping 10.10.1.1 -n {n}")
-    # ping1.start()
+    ping1 = Process(name="Ping1", exec_path=f"ping 1.1.1.1 -n {n}")
+    ping1.start()
 
     # ping2 = Process(name="Ping2", exec_path=f"piadng 10.10.0.1 -n {n}")
     # ping2.start()
     #
-    # time.sleep(2)
+    time.sleep(4)
     #
-    # ping1.stop()
+    ping1.stop()
     # ping2.stop()
 
-    ping1.update()
+    # ping1.update()
